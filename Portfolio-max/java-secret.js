15597c59f368a03510beeaed4ef6d2d1a9eba789
maxlachoucroute <maxime.huds@gmail.com>
2023-03-13 23:38:52 +0100

    on avance un peu

---
+++ Portfolio-max/java-secret.js
@@ -0,0 +1,29 @@
+// Tableau des chemins d'accès des images
+var images = [
+    "assets/images/1.png",
+    "assets/images/0.png",
+    
+    // Ajouter d'autres chemins d'accès ici
+  ];
+  
+  // Fonction pour générer une image aléatoire
+  function getRandomImage() {
+    var index = Math.floor(Math.random() * images.length);
+    return images[index];
+  }
+  
+  // Fonction pour créer et afficher une image flottante
+  function createFloatingImage() {
+    var img = document.createElement("img");
+    img.src = getRandomImage();
+    img.style.position = "fixed";
+    img.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+    img.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+    img.style.opacity = "0.7";
+    img.style.pointerEvents = "none";
+    document.body.appendChild(img);
+  }
+  
+  // Appel de la fonction pour créer des images flottantes toutes les 3 secondes
+  setInterval(createFloatingImage, 3000);
+  
