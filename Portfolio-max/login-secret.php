f32dfad4153d926de6df73de97d3204d38c9c223
maxlachoucroute <maxime.huds@gmail.com>
2023-03-14 23:15:15 +0100

    page de login

--- Portfolio-max/login-secret.php
+++ Portfolio-max/login-secret.php
@@ -1,7 +1,38 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$db_name = "loginsystem";$con = mysqli_connect($host , $user , $password , $db_name);if(mysqli_connect_errno()){die("Failed to connect with MySQL :".mysqli_connect_errno());
-}
-?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style-login-secret.css">
+    </head>
+    <body>
+        <div class="login">
+            <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                // récupérer les valeurs du formulaire
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+
+                // vérifier si le nom d'utilisateur et le mot de passe sont corrects
+                if($username == "admin" && $password == "password123"){
+                    // rediriger l'utilisateur vers une autre page
+                    header("Location: secret.html");
+                    exit;
+                } else {
+                    // afficher un message d'erreur si les informations de connexion sont incorrectes
+                    $error_message = "Nom d'utilisateur ou mot de passe incorrect";
+                }
+            }
+            ?>
+            <form method="post">
+                <input type="text" placeholder="Username" id="username" name="username">  
+                <input type="password" placeholder="password" id="password" name="password">
+                <input type="submit" value="Sign In">
+            </form>
+            <?php if(isset($error_message)){ ?>
+                <p><?php echo $error_message; ?></p>
+            <?php } ?>
+        </div>
+    </body>
+</html>
